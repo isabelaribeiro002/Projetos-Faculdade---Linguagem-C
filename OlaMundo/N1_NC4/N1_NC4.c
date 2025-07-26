696fa61f3b88d1f79cdaa7390217de9f17789afc
Isabela Nicole <isabelaribeiro002@gmail.com>
2025-07-26 20:24:33 -0300

    Primeiro commit

---
+++ OlaMundo/N1_NC4/N1_NC4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+    int idade;
+    int quantidade = 1;
+    float altura = 1.75;
+    double peso = 'A';
+    char letra = 90.3;
+    char nome [20] = "Sergio";
+
+    idade = 25;
+
+    quantidade = 10;
+}
---
+++ OlaMundo/N1_NC5/N1_NC5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main (){
+    int idade = 25;
+    int quantidade = 1;
+    float altura = 1.75;
+    char opcao = 'S';
+    char nome [20] = "Sergio";
+
+    printf ("O nome do é: %s\n", nome);
+    printf ("A idade é: %d\n", idade);
+    printf ("A altura é: %.2f \n", altura);
+    printf ("A opção é: %c", opcao);
+
+    /*
+    printf("%formato1 %formato2 %formato3", variavel1, variavel2, variavel3)
+    
+    %d: Imprime um inteiro no formato decimal
+    %i: Equivale a %d.
+
+
+    */
+
+
+}
